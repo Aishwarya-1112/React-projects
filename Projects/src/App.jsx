0c9b8e999ba5801799e9f398838f9947b6ff7b5e
Aishwarya1155 <aishwaryamanoharann@gmail.com>
2025-03-24 21:26:55 +0530

    Counter app completed

--- Projects/src/App.jsx
+++ Projects/src/App.jsx
@@ -3,11 +3,13 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 import Todo from "./Projects/Todo";
+import Counter from "./Projects/Counter";
 
 function App() {
   return (
     <>
       <Todo />
+      <Counter />
     </>
   );
 }
