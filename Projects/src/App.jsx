3550e81c2862e70f9867cb7534a4bfd9a462f1ea
Aishwarya1155 <aishwaryamanoharann@gmail.com>
2025-03-25 21:44:27 +0530

    Api call

--- Projects/src/App.jsx
+++ Projects/src/App.jsx
@@ -4,12 +4,14 @@ import viteLogo from "/vite.svg";
 import "./App.css";
 import Todo from "./Projects/Todo";
 import Counter from "./Projects/Counter";
+import Fetch from "./Projects/Fetch";
 
 function App() {
   return (
     <>
       <Todo />
       <Counter />
+      <Fetch />
     </>
   );
 }
---
+++ Projects/src/Projects/Fetch.jsx
@@ -0,0 +1,31 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+const Fetch = () => {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(
+          "https://jsonplaceholder.typicode.com/posts"
+        );
+        setData(response.data);
+      } catch (error) {
+        console.error("Error:", error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  return (
+    <div>
+      {data.map((item) => (
+        <div key={item.id}>{item.title}</div>
+      ))}
+    </div>
+  );
+};
+
+export default Fetch;
