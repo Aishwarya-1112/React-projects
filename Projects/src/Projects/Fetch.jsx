b6d2289a4da322aceb41f1c25a3e1257180b5d96
Aishwarya1155 <aishwaryamanoharann@gmail.com>
2025-03-26 21:55:58 +0530

    update fetch api

--- Projects/src/Projects/Fetch.jsx
+++ Projects/src/Projects/Fetch.jsx
@@ -29,3 +29,33 @@ const Fetch = () => {
 };
 
 export default Fetch;
+
+// const Fetch = () => {
+//   const [data, setData] = useState([]);
+
+//   useEffect(() => {
+//     const fetchData = async () => {
+//       try {
+//         const response = await axios.get(
+//           "https://jsonplaceholder.typicode.com/posts"
+
+//         );
+//         setData(response.data);
+//       } catch (error) {
+//         console.error("Error:", error);
+//       }
+//     };
+
+//     fetchData();
+//   }, []);
+
+//   return (
+//     <div>
+//       {data.map((item) => (
+//         <div key={item.id}>{item.title}</div>
+//       ))}
+//     </div>
+//   );
+// };
+
+// export default Fetch;
